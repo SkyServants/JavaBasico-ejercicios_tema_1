462d0ef0f8d03bde04c26e151f3a5343e09a5742
SkyServants <alexandermendivelso1210@gmail.com>
2023-03-22 20:01:11 -0500

    creacion Java basico ejercicios tema 1

---
+++ src/com/SkyServants/MainPrueba.java
@@ -0,0 +1,49 @@
+package com.SkyServants;
+
+public class MainPrueba {
+
+    public static void main(String[] args){
+        //Tipos de variables
+        System.out.println("Tipos de vriables en JAVA");
+
+        //Numeros
+
+        //Enteros
+        byte numero1 = 1;
+        System.out.println("Variable de tipo byte: " + numero1);
+
+        short numero2 = 5;
+        System.out.println("Variable tipo short: " + numero2);
+
+        int numero3 = 10;
+        System.out.println("Variable tipo int: " + numero3);
+
+        long numero4 = 1234567891;
+        System.out.println("Variable tipo long: " + numero4);
+
+        //Decimales
+        float numero5 = 12.15f;
+        System.out.println("Variable tipo float: " + numero5);
+
+        double numero6 = 321.345d;
+        System.out.println("Variable tipo double: " + numero6);
+
+        //texto
+        char caracter1 = 'e';
+        System.out.println("Variable tipo char: " + caracter1);
+
+        String texto1 = "Intenta de nuevo, falla de nuevo, Falla mejor";
+        System.out.println("cadena de texto (String): " + texto1);
+
+        //Boleanos
+        boolean variable1 = true;
+        System.out.println("Variable booleana para (SI): " + variable1);
+
+        boolean variable2 = false;
+        System.out.println("Variable booleana para (NO): " + variable2);
+
+
+
+
+    }
+}
